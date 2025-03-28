e475bd38ac5c98505f362b35ff894dbac0432dc3
Thomasbrockmansvives <thomas.brockmans@student.vives.be>
2025-03-28 17:17:05 +0100

    Add project files.

---
+++ SchruteAirways/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchruteAirways.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
